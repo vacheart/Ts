d93de33a7c83cbb96011f5e7391d230401938ce9
gerrit <vacheart@163.com>
2019-08-20 23:59:23 +0800

    add cmake test

---
+++ cmake/main.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: vach
+	> Mail: vacheart@163.com 
+	> Created Time: 二  8/20 23:55:45 2019
+ ************************************************************************/
+#include <stdio.h>
+int main()
+{
+ printf(“Hello World from t1 Main!n”);
+ return 0;
+}
